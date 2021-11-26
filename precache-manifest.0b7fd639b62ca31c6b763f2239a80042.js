75a41fea635f4918ee65e6d70e415f2c68900263
killerkc12 <kiranchavankc12@gmail.com>
2021-11-26 18:51:20 +0530

    Updates

---
+++ precache-manifest.0b7fd639b62ca31c6b763f2239a80042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da67446a7107e2bd1c56e414d90ddfc7",
+    "url": "/Weather-App-PWA/index.html"
+  },
+  {
+    "revision": "ef1e9c4bff3d4c78ef01",
+    "url": "/Weather-App-PWA/static/css/main.0527dc8c.chunk.css"
+  },
+  {
+    "revision": "c1b7013b4ebf1d38c63e",
+    "url": "/Weather-App-PWA/static/js/2.49ab122c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Weather-App-PWA/static/js/2.49ab122c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef1e9c4bff3d4c78ef01",
+    "url": "/Weather-App-PWA/static/js/main.33b38223.chunk.js"
+  },
+  {
+    "revision": "d521efcec52644e8b070",
+    "url": "/Weather-App-PWA/static/js/runtime-main.ad551b02.js"
+  }
+]);
